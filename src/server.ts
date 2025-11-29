90d6c6b959a8c8f04c005be0d03cb22987162760
SajidSojib <sajidahmed2401@gmail.com>
2025-11-29 22:31:03 +0600

    get post method in express

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,15 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+// app.use(express.urlencoded()));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.post('/users', (req: Request, res: Response) => {
+  res.status(201).json(req.body);
 });
 
 app.listen(port, () => {
