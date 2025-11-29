cdc2de776a911ff643183b1591d0c76086c33a60
SajidSojib <sajidahmed2401@gmail.com>
2025-11-30 02:02:52 +0600

    postgress setup done

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,40 @@
 import express, { Request, Response } from "express";
+import {Pool} from 'pg';
+import dotenv from 'dotenv';
+import path from 'path';
 
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
+
+// middlewires
 app.use(express.json());
 // app.use(express.urlencoded()));
+
+
+// db
+const pool = new Pool({
+    connectionString: `${process.env.DATABASE_URL}`,
+    ssl: { rejectUnauthorized: false },
+})
+
+const initDB = async () => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(75) NOT NULL,
+            email VARCHAR(150) NOT NULL UNIQUE,
+            age INT,
+            phone VARCHAR(20),
+            address TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT NOW()
+        )`);
+};
+
+initDB();
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
