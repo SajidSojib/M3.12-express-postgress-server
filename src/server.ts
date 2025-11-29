620d1891e41de0743a690469124bdad988639a92
SajidSojib <sajidahmed2401@gmail.com>
2025-11-30 02:59:21 +0600

    getting all and single user with postgress

--- src/server.ts
+++ src/server.ts
@@ -66,6 +66,29 @@ app.post('/users', async (req: Request, res: Response) => {
     }
 });
 
+app.get('/users', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.status(200).json({ success: true, message: 'Users fetched successfully', data: result.rows });
+    } catch (error: any) {
+        res.status(500).json({ success: false, message: error?.message });
+    }
+});
+
+app.get('/users/:id', async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+        if (result.rows.length === 0) {
+            res.status(404).json({ success: false, message: 'User not found' });
+        } else {
+            res.status(200).json({ success: true, message: 'User fetched successfully', data: result.rows[0] });
+        }
+    } catch (error: any) {
+        res.status(500).json({ success: false, message: error?.message });
+    }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
